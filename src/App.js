d5522f25fdc365ed912ac374bcec52a794fdf6d2
pavliksedoy <pavliksedoy@gmail.com>
2021-10-19 22:18:04 +0300

    Get data from api

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import React, {useState, useEffect} from 'react'
+import axios from 'axios'
 
 import Heading from './components/Heading'
 import Gallery from './components/Gallery'
@@ -9,7 +10,13 @@ export default function App() {
 
   useEffect(() => {
     setLoading(true)
-    
+    axios.get('http://jsonplaceholder.typicode.com/photos')
+      .then(res => {
+        res.data.length > 0 && setImages(res.data)
+        // console.log("🚀 ~ file: App.js ~ line 15 ~ useEffect ~ res", res)
+        setLoading(false)
+      })
+      .catch(err => console.error(err))
   }, [])
 
   return (
--- src/components/Gallery.js
+++ src/components/Gallery.js
@@ -14,7 +14,7 @@ export default function Gallery({ images, loading }) {
         </Container>
     )
 
-    else if ( images.length == 0 ) return (
+    else if ( images.length === 0 ) return (
         <Container maxWidth="sm">
             <Typography variant="h5" align="center" color="text.secondary" paragraph>
               Gallery is empty
@@ -26,7 +26,7 @@ export default function Gallery({ images, loading }) {
         <Container sx={{ py: 8 }} maxWidth="md">
             <Grid container spacing={4}>
                 {images.map(image => (
-                    <Grid item key={image} xs={12} sm={6} md={3}>
+                    <Grid item key={image.id} xs={12} sm={6} md={3}>
                         <Card sx={{ height: '100%', display: 'flex', flexDirection: 'column' }}>
                             Some image
                         </Card>
