c4b4746fefa485fd930d24f15f5aaa18b4788ebb
pavliksedoy <pavliksedoy@gmail.com>
2021-10-19 23:59:36 +0300

    Modal with full width image

--- src/components/Gallery.js
+++ src/components/Gallery.js
@@ -1,9 +1,22 @@
-import React from 'react'
+import React, {useState} from 'react'
 
-import { Typography, Card, Grid, Container, CardMedia, Button } from '@mui/material'
+import { Typography, Card, Grid, Container, CardMedia, Button, Modal, Box } from '@mui/material'
 
 export default function Gallery({ images, loading, handleRemove }) {
-    // const [currentImages, setCurrentImages] = 
+    const [modalState, setModalState] = useState(false),
+        [modalImg, setModalImg] = useState(null)
+
+    const style = {
+        position: 'absolute',
+        top: '50%',
+        left: '50%',
+        transform: 'translate(-50%, -50%)',
+        width: 400,
+        bgcolor: 'background.paper',
+        border: '2px solid #000',
+        boxShadow: 24,
+        p: 4,
+    }
 
     if (loading) return (
         <Container maxWidth="sm">
@@ -25,26 +38,57 @@ export default function Gallery({ images, loading, handleRemove }) {
         handleRemove(id)
     }
 
+    const handleCloseModal = () => {
+        setModalImg(null)
+        setModalState(false)
+    }
+
+    const handleOpenModal = imageUrl => {
+        setModalImg(imageUrl)
+        setModalState(true)
+    }
+
     return (
-        <Container sx={{ py: 8 }} maxWidth="md">
-            <Grid container spacing={4}>
-                {images.map((image, key) => (
-                    <Grid item key={image.id} xs={12} sm={6} md={3}>
-                        <Card sx={{ height: '100%', display: 'flex', flexDirection: 'column' }}>
-                        <CardMedia
-                            component="img"
-                            sx={{
-                            }}
-                            image={image.thumbnailUrl}
-                            alt={image.title}
-                        />
-                        <Button variant="outlined" color="error" onClick={() => handleRemoveImg(image.id)}>
-                            Remove
-                        </Button>
-                        </Card>
-                    </Grid>
-                ))}
-            </Grid>
-        </Container>
+        <>
+            <Container sx={{ py: 8 }} maxWidth="md">
+                <Grid container spacing={4}>
+                    {images.map((image, key) => (
+                        <Grid item key={image.id} xs={12} sm={6} md={3}>
+                            <Card
+                                sx={{ height: '100%', display: 'flex', flexDirection: 'column' }}
+                                onClick={() => handleOpenModal(image.url)}
+                            >
+                                <CardMedia
+                                    component="img"
+                                    sx={{
+                                    }}
+                                    image={image.thumbnailUrl}
+                                    alt={image.title}
+                                />
+                                <Button variant="outlined" color="error" onClick={() => handleRemoveImg(image.id)}>
+                                    Remove
+                                </Button>
+                            </Card>
+                        </Grid>
+                    ))}
+                </Grid>
+            </Container>
+            <Modal
+                open={modalState}
+                onClose={handleCloseModal}
+                aria-labelledby="modal-modal-title"
+                aria-describedby="modal-modal-description"
+            >
+                <Box sx={style}>
+                    <CardMedia
+                        component="img"
+                        sx={{
+                        }}
+                        image={modalImg}
+                        alt='some alt'
+                    />
+                </Box>
+            </Modal>
+        </>
     )
 }
