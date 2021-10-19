dc558eff28b2889cb96c7da24d999cfb1bd80605
pavliksedoy <pavliksedoy@gmail.com>
2021-10-19 22:00:34 +0300

    General title

--- src/App.js
+++ src/App.js
@@ -3,13 +3,33 @@ import React from 'react'
 import Container from '@mui/material/Container'
 import Grid from '@mui/material/Grid'
 import Card from '@mui/material/Card'
+import Box from '@mui/material/Box'
+import Typography from '@mui/material/Typography'
 
 export default function App() {
   const images = [1,2,3,4,5,6,7,8,9,10,11,12]
 
   return (
     <div>
-      <h1>Gallery</h1>
+      <Box
+          sx={{
+            bgcolor: 'background.paper',
+            pt: 8,
+            pb: 6,
+          }}
+        >
+          <Container maxWidth="sm">
+            <Typography
+              component="h1"
+              variant="h2"
+              align="center"
+              color="text.primary"
+              gutterBottom
+            >
+              Image Gallery
+            </Typography>
+        </Container>
+      </Box>
       <Container sx={{ py: 8 }} maxWidth="md">
         <Grid container spacing={4}>
           {images.map(image => (
