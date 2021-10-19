0d90ac5a68a94da195991990a9a471a0eb2fae2b
pavliksedoy <pavliksedoy@gmail.com>
2021-10-19 21:58:22 +0300

    Images grid

--- src/App.js
+++ src/App.js
@@ -1,9 +1,26 @@
 import React from 'react'
 
+import Container from '@mui/material/Container'
+import Grid from '@mui/material/Grid'
+import Card from '@mui/material/Card'
+
 export default function App() {
+  const images = [1,2,3,4,5,6,7,8,9,10,11,12]
+
   return (
     <div>
-      New App
+      <h1>Gallery</h1>
+      <Container sx={{ py: 8 }} maxWidth="md">
+        <Grid container spacing={4}>
+          {images.map(image => (
+            <Grid item key={image} xs={12} sm={6} md={3}>
+              <Card sx={{ height: '100%', display: 'flex', flexDirection: 'column' }}>
+                Some image
+              </Card>
+            </Grid>
+          ))}
+        </Grid>
+      </Container>
     </div>
   )
 }
